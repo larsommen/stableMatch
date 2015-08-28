4753e0a57aeb4309f290560cf145c9fb7eb38072
Lars Ommen <thlo@itu.dk>
2015-08-28 14:17:20 +0200

    Cleaned up code

--- GS.java
+++ GS.java
@@ -19,8 +19,32 @@ public class GS{
 	int[] nextFemale;
 	ResizingArrayQueue freeMales;
 
+	int n;
+
 
 	public GS() {
+		
+		
+		this.parsefile();
+
+		match = new int [n];
+
+		for(int i =0; i<n;i++){
+			match[i]=-1;
+		}
+
+		currentPref =new int [n];
+		nextFemale= new int [n]; 
+		freeMales = new ResizingArrayQueue();
+
+		for (int i=0; i<n;i++){
+			freeMales.enqueue(i);
+		}
+
+	}
+
+	public void parsefile(){
+
 		String line =StdIn.readLine();
 
 		while(line.charAt(0)=='#'){
@@ -29,19 +53,16 @@ public class GS{
 
 		String number =line;
 
-		int n = Integer.valueOf(number.substring(2));
+		n = Integer.valueOf(number.substring(2));
 
-		//System.out.println(" n is: "+ n);
 
 		names = new String[2*n];
 
-		//System.out.println();
 
 		for (int i=0; i<2*n; i++){
 			line=StdIn.readLine();
 			String[] parts = line.split(" ");
 			names[i]=parts[1];
-		//	System.out.println(names[i]+ i);
 		}
 
 			StdIn.readLine();
@@ -59,64 +80,32 @@ public class GS{
 
 			for(int i=0; i<prefparts.length-1;i++){
 				if(id%2==0){
-					//female
 					
 					femaleprefs[(id/2)-1] [(Integer.valueOf(prefparts[i+1])-1)/2]= i;
-				//	System.out.println("Ran female "+ id);
 
 				}else{
-					//male
 					
 					maleprefs[(id-1)/2][i]=((Integer.valueOf(prefparts[i+1]))/2)-1;
-				//	System.out.println("Updated maleprefs: "+maleprefs[(id-1)/2][i]);
-				//	System.out.println("Ran male "+id);
 
 				}
 			}
-		
 		}
-
-
-		//Done passing
-
-
-
-			//Do maching
-
-		match = new int [n];
-
-		for(int i =0; i<n;i++){
-			match[i]=-1;
-		}
-
-		currentPref =new int [n];
-		nextFemale= new int [n]; 
-		freeMales = new ResizingArrayQueue();
-
-		for (int i=0; i<n;i++){
-			freeMales.enqueue(i);
-		//	StdOut.println("enqued:" + i);
-		}
-
 	}
 
 
 	public  int getFreeMan (){
 		int freeMan = (int)freeMales.dequeue();
-		//StdOut.println("getFreeMan ran " + freeMan);
 		return freeMan;
 	}
 
 	public  int getNextWoman (int manToPropose) {
 		int womanNumber = nextFemale[manToPropose];
 		int nextWoman = maleprefs[manToPropose][womanNumber];
-		//StdOut.println("getNextWoman ran " + nextWoman);
 		nextFemale[manToPropose]++;
 		return nextWoman;
 	}
 
 	public boolean isMarried (int wantedWoman) {
-		//StdOut.println("isMarried ran " + match[wantedWoman]);
 		if (match[wantedWoman] == -1) {
 			return false;
 		}
@@ -126,7 +115,6 @@ public class GS{
 	public boolean isBetter (int newMan, int woman) {
 		int oldManPref = currentPref[woman];
 		int newManPref = femaleprefs[woman][newMan];
-		//StdOut.println("isBetter ran with new " + newManPref + " and old " + oldManPref);
 		if (newManPref < oldManPref) {
 			return true;
 		} else return false;
@@ -137,13 +125,11 @@ public class GS{
 		match[woman] = newMan;
 		currentPref[woman] = femaleprefs[woman][newMan];
 		freeMales.enqueue(oldMan);
-	//	StdOut.println("Enqueued old man:" + oldMan);
 	}
 
 	public void getMarried (int newMan, int woman) {
 		match[woman] = newMan;
 		currentPref[woman] = femaleprefs[woman][newMan];
-	//	StdOut.println("getMarried ran " + match[woman]);
 	}
 
 	public static void main(String[] args) {
@@ -172,9 +158,6 @@ public class GS{
 			}
 		}
 
-		//for (int i = 0; i<gs.match.length; i++){
-		//	StdOut.println(i + " -- " + gs.match[i]);
-		//}
 
 		for(int i=0; i<gs.match.length;i++){
 			int countfemale=(1+2*i);
